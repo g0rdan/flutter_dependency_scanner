b11de5fef26b073d82503442d6b6d7e5e4b7c036
Denis <gordin.dan@gmail.com>
2023-09-10 16:36:03 -0400

    chore: init empty repo

---
+++ example/dependency_scanner_example.dart
@@ -0,0 +1,6 @@
+import 'package:dependency_scanner/dependency_scanner.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dependency_scanner.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dependency_scanner;
+
+export 'src/dependency_scanner_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
